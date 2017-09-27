b037626c08314c287ce8abc8c5a2a03907ea85a1
Yuto INOUE <inoue_yuto@worksap.co.jp>
2017-09-27 19:19:13 +0900

    Refactored and fixed indenting of lambda expression of java-mode.

--- .emacs.d/inits/21_java_config.el
+++ .emacs.d/inits/21_java_config.el
@@ -1,32 +1,37 @@
-(if (string= window-system "w32")
-    (add-hook 'java-mode-hook
-              (lambda()
-                (c-set-style "stroustrup")
-                (c-set-offset 'inline-open 0)
-                (c-set-offset 'topmost-intro 0)
-                (c-set-offset 'defun-block-intro '+)
-                (c-set-offset 'statement 0)
-                (c-set-offset 'statement-block-intro '+)
-                (c-toggle-hungry-state 1)
-                (setq tab-width 4)
-                (setq c-basic-offset tab-width)
-                (setq indent-tabs-mode t)
-                ))
+(defadvice c-lineup-arglist (around my activate)
+  "Improve indentation of continued Java 8 lambda function opened as argument."
+  (setq ad-return-value
+        (if (and (equal major-mode 'java-mode)
+                 (ignore-errors
+                   (save-excursion
+                     (goto-char (c-langelem-pos langelem))
+                     ;; Detect "->{"
+                     ;;   and with unclosed brace.
+                     (looking-at ".*[ \t]*->[ \t]*{[^}]*$"))))
+            0                           ; no additional indent
+          ad-do-it)))
+
+(add-hook 'java-mode-hook
+          (lambda()
+            (c-set-style "stroustrup")
+            (c-set-offset 'inline-open 0)
+            (c-set-offset 'topmost-intro 0)
+            (c-set-offset 'defun-block-intro '+)
+            (c-set-offset 'statement 0)
+            (c-set-offset 'statement-block-intro '+)
+            (c-toggle-hungry-state 1)
+            (setq tab-width 4)
+            (setq c-basic-offset tab-width)
+            (setq indent-tabs-mode t)
+            ))
+
+(unless (string= window-system "w32")
   (progn
      (require 'meghanada)
      (add-hook 'java-mode-hook
                (lambda()
                   ;; meghanada-mode on
                   (meghanada-mode t)
-                  (c-set-style "stroustrup")
-                  (c-set-offset 'inline-open 0)
-                  (c-set-offset 'topmost-intro 0)
-                  (c-set-offset 'defun-block-intro '+)
-                  (c-set-offset 'statement 0)
-                  (c-set-offset 'statement-block-intro '+)
-                  (c-toggle-hungry-state 1)
-                  (setq tab-width 4)
-                  (setq c-basic-offset tab-width)
                   ))
      )
   )
