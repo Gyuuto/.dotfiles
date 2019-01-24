a5b22bd3209156d71255281f889fef656bd85abc
Gyuuto <gyuuto@gmail.com>
2019-01-24 16:36:19 +0900

    Updated flycheck config.

--- .emacs.d/inits/20_flycheck_config.el
+++ .emacs.d/inits/20_flycheck_config.el
@@ -10,10 +10,12 @@
   (define-key flycheck-mode-map (kbd "C-M-b") 'flycheck-previous-error)
   (add-hook 'c-mode-common-hook 'flycheck-mode))
 
-(eval-after-load "flycheck"
+(eval-after-load 'flycheck
   '(progn
      (when (locate-library "flycheck-irony")
-       (flycheck-irony-setup))))
+       (flycheck-irony-setup))
+     (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+  )
 
-(with-eval-after-load 'rust-mode
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+;; (with-eval-after-load 'rust-mode
+;;   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
--- .emacs.d/inits/21_cpp_config.el
+++ .emacs.d/inits/21_cpp_config.el
@@ -50,17 +50,17 @@
   (add-to-list 'company-backends 'company-irony)
   )
 
-;; (require 'flycheck)
-;; (flycheck-define-checker c/c++
-;;   "A C/C++ checker using g++."
-;;   :command ("g++" "-Wall" "-Wextra" source)
-;;   :error-patterns  ((error line-start
-;;                            (file-name) ":" line ":" column ":" " エラー: " (message)
-;;                            line-end)
-;;                     (warning line-start
-;;                            (file-name) ":" line ":" column ":" " 警告: " (message)
-;;                            line-end))
-;;   :modes (c-mode c++-mode))
+(require 'flycheck)
+(flycheck-define-checker c/c++
+  "A C/C++ checker using g++."
+  :command ("g++" "-Wall" "-Wextra" source)
+  :error-patterns  ((error line-start
+                           (file-name) ":" line ":" column ":" " エラー: " (message)
+                           line-end)
+                    (warning line-start
+                           (file-name) ":" line ":" column ":" " 警告: " (message)
+                           line-end))
+  :modes (c++-mode))
 
 ;; テンプレート
 (require 'autoinsert)
