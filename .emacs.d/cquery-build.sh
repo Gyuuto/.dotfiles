d86dd92bfd31e3d6f44942229c960596ae7dad03
Gyuuto <gyuuto@gmail.com>
2019-02-21 05:32:24 +0900

    Modified to able to run twice or more.

--- .emacs.d/cquery-build.sh
+++ .emacs.d/cquery-build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-git clone --recursive https://github.com/cquery-project/cquery.git
+if [ -d ./cquery ]; then
+    git clone --recursive https://github.com/cquery-project/cquery.git
+fi
+
 cd cquery
 git submodule update --init
-mkdir build && cd build
+mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.emacs.d/cquery -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 cmake --build .
 cmake --build . --target install
