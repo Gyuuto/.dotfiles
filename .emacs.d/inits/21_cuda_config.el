cf402f30fed0e5657c341a09d320d0df04c0661b
Gyuuto <gyuuto@gmail.com>
2020-01-17 15:00:41 +0900

    Added cuda-mode config.

---
+++ .emacs.d/inits/21_cuda_config.el
@@ -0,0 +1,34 @@
+;;; maybe this is for c++11 syntax
+(defun cuda-mode-hooks()
+  (font-lock-add-keywords
+    nil '(
+      ("\\<\\(alignof\\|alignas\\|constexpr\\|decltype\\|noexcept\\|nullptr\\|static_assert\\|thread_local\\|override\\|final\\)\\>" . 'font-lock-keyword-face)
+      )))
+
+(defadvice c-lineup-arglist (around my activate)
+  "Improve indentation of continued C++11 lambda function opened as argument."
+  (setq ad-return-value
+        (if (and (equal major-mode 'cuda-mode)
+                 (ignore-errors
+                   (save-excursion
+                     (goto-char (c-langelem-pos langelem))
+                     ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+                     ;;   and with unclosed brace.
+                     (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+            0                           ; no additional indent
+          ad-do-it)))                   ; default behavior
+
+(add-hook 'cuda-mode-hook
+          (lambda()
+            (c-set-style "stroustrup")
+            (c-set-offset 'inline-open 0)
+            (c-set-offset 'topmost-intro 0)
+            (c-set-offset 'defun-block-intro '+)
+            (c-set-offset 'statement 0)
+            (c-set-offset 'statement-block-intro '+)
+            (c-toggle-hungry-state 1)
+            (setq tab-width 4)
+            (setq c-basic-offset tab-width)
+            'cuda-mode-hooks
+            (run-hooks 'prog-mode-hook) ;; add cuda-mode to prog-mode
+            ))
