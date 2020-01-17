31af78c3705e2034ca2c921a52e3195da62fe151
Gyuuto <gyuuto@gmail.com>
2020-01-17 14:07:10 +0900

    Added check signature skip.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,6 +15,8 @@
 ;(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+;; for avoiding gnu signature check (spin?)
+; (setq package-check-signature nil)
 (package-initialize)
 
 
