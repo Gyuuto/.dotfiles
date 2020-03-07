8738ee91ceb920b2d5e4ca7ed88ccc179b6f8357
Gyuuto <gyuuto@gmail.com>
2020-03-08 00:40:49 +0900

    Added target to build cquery.

--- .emacs.d/cquery-build.sh
+++ .emacs.d/cquery-build.sh
@@ -7,6 +7,6 @@ fi
 cd cquery
 git submodule update --init
 mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.emacs.d/cquery -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.emacs.d/cquery -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -DCMAKE_GENERATOR_PLATFORM=x64
 cmake --build .
 cmake --build . --target install
