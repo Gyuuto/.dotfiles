095fdfc51300aa7b107e4ad011c97fbcc357b40a
Gyuuto <gyuuto@gmail.com>
2020-06-02 22:47:24 +0900

    Fixed comment out.

--- .emacs.d/ccls-build.sh
+++ .emacs.d/ccls-build.sh
@@ -8,7 +8,7 @@ cd ccls
 git submodule update --init
 mkdir -p build && cd build
 wget -nc -c http://releases.llvm.org/8.0.0/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
-#tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+tar xf clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/.emacs.d/ccls -DCMAKE_PREFIX_PATH=$PWD/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-18.04 -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 cmake --build .
 cmake --build . --target install
