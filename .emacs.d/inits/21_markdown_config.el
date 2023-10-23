07758c31439a1efc4631b796c35937f8d2e0dc8b
Gyuuto <gyuuto@gmail.com>
2023-10-23 21:50:58 +0900

    Modified to set tab width 4 in markdown-mode.

--- .emacs.d/inits/21_markdown_config.el
+++ .emacs.d/inits/21_markdown_config.el
@@ -1,2 +1,7 @@
 ;; markdown mode
 (autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
+
+(add-hook 'markdown-mode-hook
+          (lambda()
+            (setq tab-width 4)
+            ))
