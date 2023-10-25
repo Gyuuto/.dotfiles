6ea0533b44abc2229baf508258ebbf2d0a7dcbc1
Gyuuto <gyuuto@gmail.com>
2023-10-25 11:56:56 +0900

    Modified to not insert indent in namespace block on C++.

--- .emacs.d/inits/21_cpp_config.el
+++ .emacs.d/inits/21_cpp_config.el
@@ -29,6 +29,7 @@
             (c-set-offset 'statement-block-intro '+)
             (c-set-offset 'inlambda 0)
             (c-set-offset 'block-open 0)
+            (c-set-offset 'innamespace 0)
             (c-toggle-hungry-state 1)
             (setq tab-width 4)
             (setq c-basic-offset tab-width)
