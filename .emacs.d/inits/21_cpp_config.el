ab9da1f92b1ebef929600163fe099ee3fad4bdb9
Gyuuto <gyuuto@gmail.com>
2023-10-25 11:58:06 +0900

    Added procedure of eglot custom initialization.

--- .emacs.d/inits/21_cpp_config.el
+++ .emacs.d/inits/21_cpp_config.el
@@ -126,31 +126,63 @@
 ;; (require 'rtags)
 ;; (cmake-ide-setup)
 
-(require 'eglot)
-(cl-defmethod eglot-client-capabilities :around (server)
-  (let ((base (cl-call-next-method)))
-    (when (cl-find "ccls" (process-command
-                              (jsonrpc--process server))
-                   :test #'string-match)
-      (setf (cl-getf (cl-getf base :textDocument)
-                     :inactiveRegionsCapabilities)
-            '(:inactiveRegions t)))
-    base))
 
-(defvar-local eglot-clangd-inactive-region-overlays '())
+;; eglot customization
+(with-eval-after-load 'eglot
+  (progn
+    (cl-defmethod eglot-client-capabilities :around (server)
+      (let ((base (cl-call-next-method)))
+        (when (cl-find "ccls" (process-command
+                                 (jsonrpc--process server))
+                       :test #'string-match)
+          (setf (cl-getf (cl-getf base :textDocument)
+                         :inactiveRegionsCapabilities)
+                '(:inactiveRegions t)))
+        base))
 
-(cl-defmethod eglot-handle-notification
-  (_server (_method (eql textDocument/inactiveRegions))
-           &key regions textDocument &allow-other-keys)
-  (if-let* ((path (expand-file-name (eglot-uri-to-path
-                                     (cl-getf textDocument :uri))))
-            (buffer (find-buffer-visiting path)))
-      (with-current-buffer buffer
-        (mapc #'delete-overlay eglot-clangd-inactive-region-overlays)
-        (cl-loop
-         for r across regions
-         for (beg . end) = (eglot-range-region r)
-         for ov = (make-overlay beg end)
-         do
-         (overlay-put ov 'face 'shadow)
-         (push ov eglot-clangd-inactive-region-overlays)))))
+    (defvar-local eglot-clangd-inactive-region-overlays '())
+    (cl-defmethod eglot-handle-notification
+      (_server (_method (eql textDocument/inactiveRegions))
+               &key regions textDocument &allow-other-keys)
+      (if-let* ((path (expand-file-name (eglot-uri-to-path
+                                         (cl-getf textDocument :uri))))
+                (buffer (find-buffer-visiting path)))
+          (with-current-buffer buffer
+            (mapc #'delete-overlay eglot-clangd-inactive-region-overlays)
+            (cl-loop
+             for r across regions
+             for (beg . end) = (eglot-range-region r)
+             for ov = (make-overlay beg end)
+             do
+             (overlay-put ov 'face 'shadow)
+             (push ov eglot-clangd-inactive-region-overlays)))))
+
+    (defun eglot-ccls-inheritance-hierarchy (&optional derived)
+      "Show inheritance hierarchy for the thing at point.
+If DERIVED is non-nil (interactively, with prefix argument), show
+the children of class at point."
+      (interactive "P")
+      (if-let* ((res (jsonrpc-request
+                      (eglot--current-server-or-lose)
+                      :$ccls/inheritance
+                      (append (eglot--TextDocumentPositionParams)
+                              `(:derived ,(if derived t :json-false))
+                              '(:levels 100) '(:hierarchy t))))
+                (tree (list (cons 0 res))))
+          (with-help-window "*ccls inheritance*"
+            (with-current-buffer standard-output
+              (while tree
+                (pcase-let ((`(,depth . ,node) (pop tree)))
+                  (cl-destructuring-bind (&key uri range) (plist-get node :location)
+                    (insert (make-string depth ?\ ) (plist-get node :name) "\n")
+                    (make-text-button (+ (point-at-bol 0) depth) (point-at-eol 0)
+                                      'action (lambda (_arg)
+                                                (interactive)
+                                                (find-file (eglot--uri-to-path uri))
+                                                (goto-char (car (eglot--range-region range)))))
+                    (cl-loop for child across (plist-get node :children)
+                             do (push (cons (1+ depth) child) tree)))))))
+        (eglot--error "Hierarchy unavailable")))
+
+    (global-set-key (kbd "C-c _") 'eglot-ccls-inheritance-hierarchy)
+    ))
