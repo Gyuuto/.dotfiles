52fe0fd3d5f14fdc85d5f598d0fb2665354e999b
Gyuuto <gyuuto@gmail.com>
2023-12-10 00:23:53 +0900

    Modified font to UDEV Gothic.

--- .emacs.d/inits/00_lang_encoding.el
+++ .emacs.d/inits/00_lang_encoding.el
@@ -20,12 +20,17 @@
 ;; (add-to-list 'default-frame-alist '(font . "rictydiminished-12"))
 
 (when (string= window-system "w32")
-  (create-fontset-from-ascii-font "Consolas-11:weight=normal:slant=normal" nil "myfont")
-  (set-fontset-font "fontset-myfont" 'ascii "rictydiminished-11:weight=normal:slant=normal" nil 'append)
-  (add-to-list 'default-frame-alist '(font . "Consolas-11"))
+  (if (find-font (font-spec :name "UDEV Gothic"))
+      (progn
+        (create-fontset-from-ascii-font "UDEV Gothic JPDOC-11:weight=normal:slant=normal" nil "myfont"))
+    (progn
+      (create-fontset-from-ascii-font "consolas-11:weight=normal:slant=normal" nil "myfont")
+      (add-to-list 'default-frame-alist '(font . "Consolas-11"))
 
-  (set-fontset-font "fontset-myfont" 'japanese-jisx0208 "メイリオ-10:weight=normal:slant=normal" nil 'append)
-  (set-fontset-font "fontset-myfont" 'japanese-jisx0212 "メイリオ-10:weight=normal:slant=normal" nil 'append)
+      (set-fontset-font "fontset-myfont" 'japanese-jisx0208 "メイリオ-10:weight=normal:slant=normal" nil 'append)
+      (set-fontset-font "fontset-myfont" 'japanese-jisx0212 "メイリオ-10:weight=normal:slant=normal" nil 'append)
+      )
+    )
 
   (add-to-list 'default-frame-alist '(font . "fontset-myfont"))
 )
