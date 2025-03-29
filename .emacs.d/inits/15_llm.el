6b74ca7313b46f59a4add3fd83cf459be3be7fa3
Gyuuto <gyuuto@gmail.com>
2025-03-29 21:49:52 +0900

    Added some functions of ellama.

--- .emacs.d/inits/15_llm.el
+++ .emacs.d/inits/15_llm.el
@@ -25,4 +25,38 @@
 
   ;; customize display
   (push '("ellama.*\.org" :regexp t :height 0.5) popwin:special-display-config)
+
+  ;; ask selected region in Japanese
+  (defun ellama-translate-selection ()
+    (interactive)
+    (let
+        ((text (if (region-active-p)
+		           (buffer-substring-no-properties (region-beginning) (region-end))
+		         (buffer-substring-no-properties (point-min) (point-max)))))
+      (ellama-instant (format "What is %s?" text)
+                      :system "Answer in Japanese")))
+
+  ;; add a function define of selection sentence
+  (defun ellama-define-selection ()
+    "Find definition fo selection."
+    (interactive)
+    (let
+        ((text (if (region-active-p)
+		           (buffer-substring-no-properties (region-beginning) (region-end))
+		         (buffer-substring-no-properties (point-min) (point-max)))))
+      (ellama-instant (format ellama-define-word-prompt-template text))))
+
+  ;; override summarize function as asnwering in Japanese
+  (defun ellama-summarize ()
+    "Summarize selected region or current buffer."
+    (interactive)
+    (let ((text (if (region-active-p)
+		            (buffer-substring-no-properties (region-beginning) (region-end))
+		          (buffer-substring-no-properties (point-min) (point-max)))))
+      (ellama-instant text
+		              :system
+		              (format "%s. Also answer in Japanese." ellama-summarize-prompt-template)
+		              :provider (or ellama-summarization-provider
+				                    ellama-provider
+				                    (ellama-get-first-ollama-chat-model)))))
   )
