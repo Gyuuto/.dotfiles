d6c50c334d7dc48cda1d2549584d7ae8a3d9bb6e
Gyuuto <gyuuto@gmail.com>
2025-06-26 02:11:57 +0900

    Modified font in codeblock of markdown mode.

--- .emacs.d/inits/21_markdown_config.el
+++ .emacs.d/inits/21_markdown_config.el
@@ -5,3 +5,6 @@
           (lambda()
             (setq tab-width 4)
             ))
+
+;; don't change font in codeblocks
+(set-face-attribute 'markdown-code-face nil :inherit 'default :foreground "#00ccff")
