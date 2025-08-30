5b71856c98e29701a7b2d4ef8af770b73ca78732
Gyuuto <gyuuto@gmail.com>
2025-08-30 17:09:05 +0900

    Modified format in cuda-mode.

--- .emacs.d/inits/21_cuda_config.el
+++ .emacs.d/inits/21_cuda_config.el
@@ -26,9 +26,13 @@
             (c-set-offset 'defun-block-intro '+)
             (c-set-offset 'statement 0)
             (c-set-offset 'statement-block-intro '+)
+            (c-set-offset 'inlambda 0)
+            (c-set-offset 'block-open 0)
+            (c-set-offset 'innamespace 0)
             (c-toggle-hungry-state 1)
             (setq tab-width 4)
             (setq c-basic-offset tab-width)
+            (yas-minor-mode)
             'cuda-mode-hooks
             (run-hooks 'prog-mode-hook) ;; add cuda-mode to prog-mode
             ))
